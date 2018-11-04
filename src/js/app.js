72f33cfc8e175bd1ee7d385e8c708922f1aa4c5d
kjameshuer <kjhuer@gmail.com>
2018-11-04 12:05:18 -0500

    new features

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,6 @@ require('../scss/styles.scss')
 // JS
 window.jQuery = require('jquery');
 require('foundation-sites/dist/js/foundation.min.js');
+require('slick-carousel/slick/slick.min.js');
+require('lightbox2');
 require('./main.js');
--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,14 @@ import 'promise-polyfill/src/polyfill';
     if ( !(typeof NodeList.prototype.forEach === "function") ){
       NodeList.prototype.forEach = Array.prototype.forEach;
     }
-
+    $('.slider').slick({
+      dots: true,
+      infinite: true,
+      prevArrow: '<button type="button" class="slick-prev">\x3c</button>',
+      nextArrow: '<button type="button" class="slick-next">\x3e</button>',
+      autoplay: true,
+      autoplaySpeed: 5000,
+    });
    
   })(jQuery);
   
