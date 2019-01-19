02751d8c041ed2ed7ae870b09cb55838abaa6077
kjameshuer <kjhuer@gmail.com>
2019-01-19 14:04:37 -0500

    security fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,16 @@
 import 'promise-polyfill/src/polyfill';
+import SETUP from './setup.js';
 
-(function ($) { 
-  
-    $('body').removeClass('no-js');
-    $(document).foundation();
-    if ( !(typeof NodeList.prototype.forEach === "function") ){
-      NodeList.prototype.forEach = Array.prototype.forEach;
-    }
-    $('.slider').slick({
-      dots: true,
-      infinite: true,
-      prevArrow: '<button type="button" class="slick-prev">\x3c</button>',
-      nextArrow: '<button type="button" class="slick-next">\x3e</button>',
-      autoplay: true,
-      autoplaySpeed: 5000,
-    });
-   
-  })(jQuery);
-  
+(function ($) {
+  SETUP.run($);
+
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev">\x3c</button>',
+    nextArrow: '<button type="button" class="slick-next">\x3e</button>',
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
+})(jQuery);
