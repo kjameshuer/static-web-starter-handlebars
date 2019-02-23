5563f58152b8171b639ddcf0ffd2440d91619392
kjameshuer <kjhuer@gmail.com>
2019-02-23 11:57:46 -0500

    oops

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,14 @@ import SETUP from './setup.js';
 
 (function ($) {
   SETUP.run($);
-  SETUP.addSlider($);
+
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev">\x3c</button>',
+    nextArrow: '<button type="button" class="slick-next">\x3e</button>',
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
 })(jQuery);
--- src/js/setup.js
+++ src/js/setup.js
@@ -1,5 +1,3 @@
-import { afterChange, beforeChange } from './slideHelper';
-
 const SETUP = {
    run($){        
         $(document).foundation();
@@ -16,29 +14,6 @@ const SETUP = {
             window.scrollTo(0, currentScrollPosition - wheelDelta);
           });
         }
-    },
-    addSlider($){
-
-      $('.slider')
-      .on('init', () =>{
-        beforeChange($);
-        afterChange($);
-      })
-      .on('afterChange', () => afterChange($))
-      .on('beforeChange',()=> beforeChange($))
-      .slick({
-        dots: true,
-        arrows: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        pauseOnFocus: true,
-        pauseOnHover: false,
-        pauseOnDotsHover: false,
-        swipe: true,
-        swipeToSlide: true,
-        touchMove: true
-      });  
     }
 }
 
